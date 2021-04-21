cfeae65981e56bad168881addde7da4b6df19389
Andrew Faber <afaber003@gmail.com>
2021-04-21 12:45:13 -0700

    simplified and cleaned up a few things

--- stack.h
+++ stack.h
@@ -12,63 +12,53 @@ using namespace std;
 
 template<typename T>
 class stack {
-
-   /* public:
-        stack(); //contructs new stack
-        void push(T val); // inserts a new element (val) of type T (T could be integer or string) into the data. If the data array is full, this function should throw an overflow_error exception with error message "Called push on full stack.".
-        void pop(); // removes the last element from data. If the data array is empty, this function should throw an outofrange exception with error message "Called pop on empty stack.".
-        T top(); // returns the top element of stack (last inserted element). If stack is empty, this function should throw an underflow_error exception with error message "Called top on empty stack.".
-        bool empty(){if(size == 0) {return true;} return false;}
-
-    private:
-        //const int MAX_SIZE = 20;
-        unsigned size; // current number of items in stack (is always one more than the top index)
-
-*/
-public:
-  stack()
-  {
-      size = 0;
-  }
-
-  bool empty(){if(size == 0) {return true;} return false;}
   
-  //template<typename T>
-  void push(T val){
-    if (size >= 20){
-      throw overflow_error("Called push on full stack.");
-    } 
-    else 
-    {
-      data[++size] = val;
-    }
-  }
+  private:
+    T data[MAX_SIZE];
+    unsigned size; // current number of items in stack (is always one more than the top index)
 
-  void pop()
-  {
-    if (empty()){
-      throw out_of_range("Called pop on empty stack.");
+
+  public:
+
+    //Constuctor
+    stack(){
+      size = 0;
     }
 
-    --size;
-  }
-
-  //template<typename T>
-  T top()
-  {
-    if (empty()){
-      throw underflow_error("Called top on empty stack.");
+    //empty() function
+    bool empty(){
+      if (size == 0){
+        return true;
+      }
+      return false;
+    }
+    
+    // Push Function
+    void push(T val){
+      if (size >= 20){
+        throw overflow_error("Called push on full stack.");
+      } 
+      else {
+        data[size] = val;
+        size++;
+      }
     }
 
-    return data[size];
+    //pop() function
+    void pop(){
+      if (empty()){
+        throw out_of_range("Called pop on empty stack.");
+      }
+      --size;
+    }
 
-  }
-
-private:
-  //const int MAX_SIZE = 20;
-  T data[MAX_SIZE];
-  unsigned size; // current number of items in stack (is always one more than the top index)
-
+    //top() function
+    T top(){
+      if (empty()){
+        throw underflow_error("Called top on empty stack.");
+      }
+      return data[size-1];
+    }
 };
 
 #endif
